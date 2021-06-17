5ca079a5644d5ef9e260214658c200b5bf990650
alexis duru <alexis.duru.pro@gmail.com>
2021-06-17 18:13:00 +0200

    Read One Create

--- src/Controller/StreamController.php
+++ src/Controller/StreamController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Track;
 use App\Repository\TrackRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,6 +18,16 @@ class StreamController extends AbstractController
     {
         return $this->render('stream/index.html.twig', [
             'tracks' => $trackRepository->findAll(),
+        ]);
+    }
+
+     /**
+     * @Route("/stream/{id}", name="app_stream_details", methods={"GET", "POST"})
+     */
+    public function show(Track $track, Request $request): Response
+    {
+        return $this->render('stream/details.html.twig', [
+            'track' => $track,
         ]);
     }
 }

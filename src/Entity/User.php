e3e78fc24e26f71bb5572ec17b39404dd3fd8eee
alexis duru <alexis.duru.pro@gmail.com>
2021-06-17 17:46:37 +0200

    Fonction ShowAll

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -66,6 +66,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->tracks = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->username;
+    }
+
     public function getId(): ?int
     {
         return $this->id;

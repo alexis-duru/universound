b38ab8295b9372d6431ade518e1936d6b3c4bb21
alexis duru <alexis.duru.pro@gmail.com>
2021-07-05 21:16:08 +0200

    update global css

--- src/Form/CommentFormType.php
+++ src/Form/CommentFormType.php
@@ -7,13 +7,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content')
+            ->add('content',TextareaType::class)
             ->add('comment', SubmitType::class)
             
         ;

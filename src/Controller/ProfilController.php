fcf5a80f7bd028554be548789f4f70d3b9b0a4a2
Alexis-Duru <alexis.duru.pro@gmail.com>
2021-07-09 10:49:26 +0200

    Role Admin / Role User

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,16 +2,16 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Entity\Comment;
+use App\Entity\User;
 use App\Form\RegistrationFormType;
-use App\Repository\TrackRepository;
 use App\Repository\CommentRepository;
+use App\Repository\TrackRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfilController extends AbstractController
 {

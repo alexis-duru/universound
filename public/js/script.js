530464da78819c2c0ed4a1dacdd14d16d6dbf7fa
alexis duru <alexis.duru.pro@gmail.com>
2021-07-22 22:57:15 +0200

    play/stop player JS

--- public/js/script.js
+++ public/js/script.js
@@ -43,7 +43,6 @@
     // });
 
 
-
     // ----------------------------------------------- //
 
     // var $audio =$('.audiotrack');
@@ -53,6 +52,15 @@
     // });
 
 })(jQuery);
+
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
 
 
 
